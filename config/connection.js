c2ffb8202e7cba5377e84bd5f3768d7ff80e9c4f
Jeorge Donato <jeorgekhenr@gmail.com>
2020-09-08 19:57:15 -0700

    Constructed views template

--- config/connection.js
+++ config/connection.js
@@ -33,31 +33,3 @@ class Database {
 }
 
 module.exports = Database;
-
-// module.exports = {
-//   connection: mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_ROOT,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT
-//   }),
-//   query: function (sql, args) {
-//     return new Promise((resolve, reject) => {
-//       this.connection.query(sql, args, (err, rows) => {
-//         if (err)
-//           return reject(err);
-//         resolve(rows);
-//       });
-//     });
-//   },
-//   close: function () {
-//     return new Promise((resolve, reject) => {
-//       this.connection.end(err => {
-//         if (err)
-//           return reject(err);
-//         resolve();
-//       });
-//     });
-//   }
-// }
--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,8 @@
 const Database = require('./connection');
+const { printQuestionMarks, objToSql } = require('../helpers/helper');
 const db = new Database();
+
+
 module.exports = orm = {
   selectAll: async function (tableInput) {
     const queryString = "SELECT * FROM " + tableInput + ";";
@@ -7,9 +10,29 @@ module.exports = orm = {
     db.close();
     return result;
   },
-  insertOne: async function (tableInput) {
+  insertOne: async function (tableInput, cols, vals) {
     const queryString = "INSER INTO " + tableInput + ";";
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+    const result = await db.query(queryString, vals);
+    console.log(queryString);
+    db.close();
+    return result;
+  },
+  updateOne: function (tableInput, objColVals, condition, cb) {
+    var queryString = "UPDATE " + tableInput;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
     const result = await db.query(queryString);
+    console.log(queryString);
     db.close();
     return result;
   },
